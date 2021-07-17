42c30ba4d8cdb68fab1683d522a03160d6065ad9
Dmytro Kurylko <71904676+Dmytro-kur@users.noreply.github.com>
2021-07-17 15:07:56 +0300

    sass

---
+++ ForceApp/force/static/force/header.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector(".wave-btn").onclick = function() {
+        // do things, and then
+        return false;
+    };
+})
