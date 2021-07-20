1f759a88b02ab120507342d9311923d31756ad61
Dmytro Kurylko <71904676+Dmytro-kur@users.noreply.github.com>
2021-07-20 23:51:42 +0300

    before button adjusting

--- ForceApp/force/static/force/header.js
+++ ForceApp/force/static/force/header.js
@@ -1,6 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector(".wave-btn").onclick = function() {
-        // do things, and then
-        return false;
+// get document coordinates of the element
+function getCoords(elem) {
+    let box = elem.getBoundingClientRect();
+  
+    return {
+      top: box.top + window.pageYOffset,
+      right: box.right + window.pageXOffset,
+      bottom: box.bottom + window.pageYOffset,
+      left: box.left + window.pageXOffset
     };
+  }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector(".wave-btn").onclick = (event) => {
+
+        const coords = getCoords(event.target);
+        const center = {
+            X: event.pageX - coords.left,
+            Y: event.pageY - coords.top
+        }
+        const element = document.querySelector('.wave-btn__waves');
+
+        // document.querySelector('.wave-btn__waves').style.clipPath = `circle(10px at ${center.X}px ${center.Y}px)`;
+        
+        element.classList.remove("wave-btn__waves");
+        void element.offsetWidth;
+        element.classList.add("wave-btn__waves");
+        return false;
+    }
+
 })
