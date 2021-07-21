b7c54675db0cad58b0b9e0fc8b01b04887896b01
Dmytro Kurylko <dimakurylko@gmail.com>
2021-07-21 11:59:16 +0300

    ssub

--- ForceApp/force/static/force/header.js
+++ ForceApp/force/static/force/header.js
@@ -20,8 +20,10 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         const element = document.querySelector('.wave-btn__waves');
 
-        // document.querySelector('.wave-btn__waves').style.clipPath = `circle(10px at ${center.X}px ${center.Y}px)`;
-        
+        document.querySelector('.wave-btn__waves').style.clipPath = `circle(0px at ${center.X}px ${center.Y}px)`;
+        document.querySelector('.wave-btn__waves').style.left = `${event.pageX-500}px`;
+        document.querySelector('.wave-btn__waves').style.top = `${event.pageY-500}px`;
+        document.querySelector('.wave-btn__waves').style.animationPlayState = 'running';
         element.classList.remove("wave-btn__waves");
         void element.offsetWidth;
         element.classList.add("wave-btn__waves");
