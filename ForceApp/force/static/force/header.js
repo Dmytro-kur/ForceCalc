245417bf6547d01d2839e42734df447ad1d93b8f
Dmytro Kurylko <dimakurylko@gmail.com>
2021-08-27 12:09:24 +0300

    user menu was aligned

--- ForceApp/force/static/force/header.js
+++ ForceApp/force/static/force/header.js
@@ -6,9 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
     if (document.querySelector('#user-menu').style.display === 'flex') {
       document.querySelector('#user-menu').style.display = 'none';
     } else if (document.querySelector('#user-menu').style.display === 'none') {
-      document.querySelector('#user-menu').style.right = `${300}px`;
-      document.querySelector('#user-menu').style.top = `${25}px`;
+      
       document.querySelector('#user-menu').style.display = 'flex';
+      document.querySelector('#user-menu').style.top = `${25}px`;
+
+      const coords = getCoords(document.querySelector('#user-menu'));
+      const elementWidth = coords.right - coords.left;
+
+      if (window.innerWidth - elementWidth < 0) {
+        document.querySelector('#user-menu').style.right = `${200}px`;
+      } else {
+        document.querySelector('#user-menu').style.right = `${elementWidth}px`;
+      }
+      
+
       document.querySelector('.change_pwd_logo_wave').style.animationPlayState = 'paused';
       document.querySelector('.logout_logo_wave').style.animationPlayState = 'paused';
     }
