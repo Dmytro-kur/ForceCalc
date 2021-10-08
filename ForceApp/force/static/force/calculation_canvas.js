30f68aaca43a2141e0e8ec6c6b5e37038d620693
Dmytro Kurylko <dimakurylko@gmail.com>
2021-10-08 14:03:02 +0300

    Update calculation_canvas.js

--- ForceApp/force/static/force/calculation_canvas.js
+++ ForceApp/force/static/force/calculation_canvas.js
@@ -1011,4 +1011,3 @@ function draw(ctx, scale, posX, posY,
 // https://www.canva.com/colors/color-palettes/
 // https://css-tricks.com/clipping-masking-css/
 // https://www.photopea.com/
-// https://www.photopea.com/
