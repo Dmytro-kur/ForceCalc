e915218e8137af190d78a66b8e3db97c0bd6abd3
Dmytro Kurylko <dimakurylko@gmail.com>
2021-10-13 14:13:39 +0300

    sett

--- ForceApp/settings.py
+++ ForceApp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,6 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# with open('SECRET_KEY/ForceCalc.txt') as f:
+#     SECRET_KEY = f.read().strip()
+
 SECRET_KEY = input('SECRET_KEY:\n')
 
 # SECURITY WARNING: don't run with debug turned on in production!
